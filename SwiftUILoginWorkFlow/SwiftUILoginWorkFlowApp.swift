c7109c7ad527ba1352eaf9868ec77f62064abbd5
yusufovski <salihyusufcankurt@hotmail.com>
2021-10-18 22:14:02 +0300

    cleaned comments

--- SwiftUILoginWorkFlow/SwiftUILoginWorkFlowApp.swift
+++ SwiftUILoginWorkFlow/SwiftUILoginWorkFlowApp.swift
@@ -12,10 +12,7 @@ struct SwiftUILoginWorkFlowApp: App {
     @StateObject var authentication = Authentication()
     var body: some Scene {
         WindowGroup {
-            if authentication.isValidated{
-//                ContentView()
-//                    .environmentObject(authentication)
-//                
+            if authentication.isValidated{              
                 TabView{
                     ContentView()
                         .tabItem {
